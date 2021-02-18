0a44fc603059c6a403c6b7041d7a531cc883f612
Flarecrow <46488915+Flarecrow@users.noreply.github.com>
2021-02-18 15:48:51 +0100

    pick up on button press fixed

--- Source/Project_Knockdome/PickUp.cpp
+++ Source/Project_Knockdome/PickUp.cpp
@@ -67,7 +67,7 @@ void APickUp::Tick(float DeltaTime)
 void APickUp::OnPlayerEnterPickUpItem(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	
-	Destroy();
+	//Destroy();
 
 }
 
